30adc4e6c4b404925ebded652a706a2ba19da35c
csteenberg <csteenberg@hotmail.co.uk>
2024-08-01 21:49:13 +0200

    Added textnode to html functionality and unit testing.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,7 +7,8 @@ from textnode import (
     text_type_image,
     text_type_italic,
     text_type_link,
-    text_type_text
+    text_type_text,
+    text_node_to_html_node
 )
 
 class TestTextNode(unittest.TestCase):
@@ -35,6 +36,27 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", text_type_text, "https://docs.python.org/3/reference/")
         self.assertEqual("TextNode(This is a text node, text, https://docs.python.org/3/reference/)", repr(node))
 
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text_to_html(self):
+        node_text = TextNode("This is a text node", text_type_text)
+        node_with_url = TextNode("Click me!", text_type_link, "https://docs.python.org/3/reference/")
+        node_with_image = TextNode("Github!", text_type_image, "github.com")
+
+        html_text = text_node_to_html_node(node_text)
+        html_url = text_node_to_html_node(node_with_url)
+        html_image = text_node_to_html_node(node_with_image)
+
+        #test node_text
+        self.assertEqual(html_text.tag, None)
+        self.assertEqual(html_text.value, "This is a text node")
+        #test node_with_url
+        self.assertEqual(html_url.tag, "a")
+        self.assertEqual(html_url.value, "Click me!")
+        self.assertEqual(html_url.props, {"href": "https://docs.python.org/3/reference/"})
+        #test node_with_image
+        self.assertEqual(html_image.tag, "img")
+        self.assertEqual(html_image.value, None)
+        self.assertEqual(html_image.props, {"src": "github.com", "alt": "Github!"})
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 text_type_text = "text"
 text_type_bold = "bold"
 text_type_italic = "italic"
@@ -20,3 +22,20 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception(f"Invalid text type: {text_node.text_type}")
