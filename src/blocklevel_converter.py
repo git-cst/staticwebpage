706ac6aceab7933e19aa02ddc8745f6d97a5922a
csteenberg <csteenberg@hotmail.co.uk>
2024-08-06 21:49:45 +0200

    Finished block_to_blocktype

--- src/blocklevel_converter.py
+++ src/blocklevel_converter.py
@@ -1,3 +1,5 @@
+import regex as re
+
 def markdown_to_blocks(markdown):
     split_blocks = []
     for line in markdown.split("\n"):
@@ -5,3 +7,41 @@ def markdown_to_blocks(markdown):
             continue
         split_blocks.append(line.strip())
     return split_blocks
+
+def block_to_blocktype(block):
+    check_heading = re.findall(r"^(#+)", block)
+    check_ordered_list = re.findall(r"^(\d+\.)", block)
+    check_unordered_list = re.findall(r"^(\*+)|^(-+)", block)
+    check_code_block = re.findall(r"^(```)|(```)$", block)
+    check_quote_block = re.findall(r"^(>)", block)
+
+    if len(check_heading) != 0:
+        return "heading"
+    if len(check_code_block) != 0:
+        return "code"
+    if len(check_quote_block) != 0:
+        return "quote"
+    if len(check_unordered_list) != 0:
+        return "unordered_list"
+    if len(check_ordered_list) != 0:
+        return "ordered_list"
+    return "paragraph"
+
+def markdown_to_html(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        current_block_type = block_to_blocktype(block)
+        match current_block_type:
+            case "paragraph":
+                pass
+            case "heading":
+                pass
+            case "code":
+                pass
+            case "quote":
+                pass
+            case "unordered list":
+                pass
+            case "ordered_list":
+                pass
+    pass 
--- src/test_blocklevel_converter.py
+++ src/test_blocklevel_converter.py
@@ -2,7 +2,8 @@ import unittest
 
 from blocklevel_converter import *
 
-class TestHTMLNode(unittest.TestCase):
+class TestBlockExtraction(unittest.TestCase):
+    #TEST BLOCK EXTRACTION
     def test_block_singleline(self):
         test_block = """
         # This is a heading
@@ -48,6 +49,44 @@ class TestHTMLNode(unittest.TestCase):
                           "- Unordered list item 3",
                           "Text in a paragraph format with a [link](https://www.google.com)"], 
                           markdown_to_blocks(test_block))
+        
+    #TEST BLOCK TYPE IDENTIFICATION
+    def test_paragraph(self):
+        test_block = "This is a text string"
+        self.assertEqual("paragraph",
+                            block_to_blocktype(test_block))
+
+    def test_heading(self):
+        test_block = "# This is a heading"
+        self.assertEqual("heading",
+                            block_to_blocktype(test_block))
+
+    def test_code(self):
+        test_block = "```This is a code block```"
+        self.assertEqual("code",
+                            block_to_blocktype(test_block))
+
+    def test_quote(self):
+        test_block = ">This is a quote"
+        self.assertEqual("quote",
+                            block_to_blocktype(test_block))
+
+    def test_unordered_list(self):
+        test_block = "* This is an unordered list"
+        test_block_2 = "- This is also an unordered list"
+        self.assertEqual("unordered_list",
+                            block_to_blocktype(test_block))
+        self.assertEqual("unordered_list",
+                            block_to_blocktype(test_block_2))
+
+    def test_ordered_list(self):
+        test_block = "1. This is an ordered list"
+        self.assertEqual("ordered_list",
+                            block_to_blocktype(test_block))
+
+    #TEST BLOCK TO HTML CONVERSIO
+    def test_block_to_html(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
